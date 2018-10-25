db9eb6cdefd363faec9d9a27b3f0982d39ecfa69
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-10-25 08:31:19 +0200

    Fix relationship graphs

--- addon/mirage-graphql/mocks/question.js
+++ addon/mirage-graphql/mocks/question.js
@@ -5,14 +5,65 @@ import {
   Serializer,
   register
 } from "ember-caluma-utils/mirage-graphql";
+import { classify } from "@ember/string";
 
 const optionFilter = new Filter("Option");
 const optionSerializer = new Serializer("Option");
 
 export default class extends BaseMock {
   @register("Question")
-  handle({ __typename }) {
+  handleQuestion(root) {
+    let questionId =
+      root.questionId || (root.node && root.node(...arguments).id);
+    let __typename = root.__typename;
+
+    if (questionId) {
+      __typename = `${classify(
+        this.collection.findBy({ id: questionId }).type.toLowerCase()
+      )}Question`;
+    }
+
     return { __typename };
+  }
+
+  handleInterfaceType(root, vars, _, meta) {
+    return this.handle.fn.call(
+      this,
+      root,
+      { ...vars, id: root.questionId },
+      _,
+      meta
+    );
+  }
+
+  @register("TextQuestion")
+  handleTextQuestion() {
+    return this.handleInterfaceType(...arguments);
+  }
+
+  @register("TextareaQuestion")
+  handleTextareaQuestion() {
+    return this.handleInterfaceType(...arguments);
+  }
+
+  @register("IntegerQuestion")
+  handleIntegerQuestion() {
+    return this.handleInterfaceType(...arguments);
+  }
+
+  @register("FloatQuestion")
+  handleFloatQuestion() {
+    return this.handleInterfaceType(...arguments);
+  }
+
+  @register("CheckboxQuestion")
+  handleChecboxQuestion() {
+    return this.handleInterfaceType(...arguments);
+  }
+
+  @register("RadioQuestion")
+  handleRadioQuestion() {
+    return this.handleInterfaceType(...arguments);
   }
 
   @register("SaveTextQuestionPayload")
